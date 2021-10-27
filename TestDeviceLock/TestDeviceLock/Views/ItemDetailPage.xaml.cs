ee52a31256c3dda0b746bd3cace8984c45df722b
Chris Marrasovich <chris.marassovich@distillerysoftware.com>
2021-10-28 10:54:33 +1100

    Add project files.

---
+++ TestDeviceLock/TestDeviceLock/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TestDeviceLock.ViewModels;
+using Xamarin.Forms;
+
+namespace TestDeviceLock.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
